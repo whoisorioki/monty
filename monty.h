e9e8e35123febba93e6395399b133a67e2312523
Bart254 <omondibartlett3@gmail.com>
2023-01-21 19:56:11 +0300

    Update header file

--- monty.h
+++ monty.h
@@ -31,12 +31,34 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-void assign_fun(instruction_t *monty, int check, int line_number);
+
+/**
+ * struct globals - global variables
+ * @stream: file to be opened
+ * @monty: instruction_t structure
+ * @head: head of list
+ * @arg: argument to be passed to opcode
+ * @buffer: memory space allocated by getline
+ *
+ * Description - this is a structure to variables
+ * that are to be accessed in different
+ * functions to be freed when program terminates
+ */
+typedef struct globals
+{
+	FILE *stream;
+	instruction_t *monty;
+	stack_t *head;
+	char *arg;
+	char *buffer;
+} globals_t;
+
+extern globals_t vars;
+void assign_fun(int check, int line_number);
 void *_malloc(size_t size);
 char *command(char *buffer);
 char *_arg(char *str);
-int valid(instruction_t *monty, int line_number);
-void add_node_beginning(stack_t **head, char *s, int l_number);
+void valid(int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number);
@@ -44,8 +66,6 @@ void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
-extern stack_t *top;
-void _free(char **, char **);
-void free_all(char *, instruction_t *);
+void free_all(void);
 
 #endif
